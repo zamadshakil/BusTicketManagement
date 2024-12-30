15c0f1166357e3ac29c7e450663c8d92c44862ac
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2024-12-30 12:37:54 +0500

    Add project files.

---
+++ BusTicketManagement.cpp
@@ -0,0 +1,84 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+struct Routes {
+	string start, end, start_end_time;
+	int fare;
+};
+int main() {
+	int login_choice;
+	cout << "Enter your cho1. Customer(Buy a Tickets)\n2. Admin(Manage Tickets)" << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*string LHR = "Lahore", ISL="Islamabad", KAR="Karachi", MLT="Multan", FAS = "Faisalabad";
+	int a = 10;
+	vector<Routes> routes = {
+		{LHR, ISL, "12:00 am to 05:00 am", 1500},
+		{LHR, KAR, "10:00 am to 05:00 pm", 3000},
+		{LHR, MLT, "11:00 pm to 05:00 am", 2000},
+		{LHR, FAS, "01:00 pm to 05:00 pm", 1000}
+	};
+	cout << routes.size() << endl;
+	for (int i = 0; i < routes.size(); i++) {
+		cout << "Route: " << routes[i].start << endl;
+		cout << "Route: " << routes[i].end << endl;
+		cout << "Time: " << routes[i].start_end_time << endl;
+		cout << "Fare: " << routes[i].fare << endl;
+		cout << endl;
+	}
+	routes.push_back({ ISL, KAR, "12:00 am to 05:00 am", 4000 });
+	cout << routes.size() << endl;
+	for (int i = 0; i < routes.size(); i++) {
+		cout << "Route: " << routes[i].start << endl;
+		cout << "Route: " << routes[i].end << endl;
+		cout << "Time: " << routes[i].start_end_time << endl;
+		cout << "Fare: " << routes[i].fare << endl;
+		cout << endl;
+	}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
